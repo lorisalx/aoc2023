fd29a129a9f6a83a8252647e9d7e4a654807dee7
Loris Alexandre <loris.alexandre@boursorama.fr>
2023-12-05 10:10:42 +0100

    feat(aoc): day5

--- day5/day5.js
+++ day5/day5.js
@@ -4,80 +4,80 @@ const input = require('fs').readFileSync('input.txt', 'utf8');
 const input1 = require('fs').readFileSync('part1.txt', 'utf8');
 
 let seeds = [];
-let seedToSoil = new Map();
-let soilToFertilizer = new Map();
-let fertilizerToWater = new Map();
-let waterToLight = new Map();
-let lightToTemperature = new Map();
-let temperatureToHumidity = new Map();
-let humidityToLocation = new Map();
+let seedToSoil = [];
+let soilToFertilizer = [];
+let fertilizerToWater = [];
+let waterToLight = [];
+let lightToTemperature = [];
+let temperatureToHumidity = [];
+let humidityToLocation = [];
 
-function xToY(x, map) {
-  let xToYMapValues = x.split(':')[1].split('\n').filter(x => x !== '');
-  xToYMapValues = xToYMapValues.map(x => x.split(' ').filter(x => x !== '').map(x => parseInt(x)));
-  xToYMapValues.forEach(x => {
-    for (i = 0; i < x[2]; i++) {
-      map.set(x[1] + i, x[0] + i);
-    }
+function saveValues(x, array) {
+  let saveValuesMapValues = x.split(':')[1].split('\n').filter(x => x !== '');
+  saveValuesMapValues = saveValuesMapValues.map(x => x.split(' ').filter(x => x !== '').map(x => parseInt(x)));
+  saveValuesMapValues.forEach(x => {
+    array.push(x);
   });
-  
 }
 const part1 = input.split('\n\n').map(x=> {
   if (x.includes('seeds:')) {
     seeds = x.split(':')[1].split(' ').filter(x => x !== '').map(x => parseInt(x));
   }
   if (x.includes('seed-to-soil')) {
-    xToY(x, seedToSoil);
+    saveValues(x, seedToSoil);
   }
   if (x.includes('soil-to-fertilizer')) {
-    xToY(x, soilToFertilizer);
+    saveValues(x, soilToFertilizer);
   }
   if (x.includes('fertilizer-to-water')) {
-    xToY(x, fertilizerToWater);
+    saveValues(x, fertilizerToWater);
   }
   if (x.includes('water-to-light')) {
-    xToY(x, waterToLight);
+    saveValues(x, waterToLight);
   }
   if (x.includes('light-to-temperature')) {
-    xToY(x, lightToTemperature);
+    saveValues(x, lightToTemperature);
   }
   if (x.includes('temperature-to-humidity')) {
-    xToY(x, temperatureToHumidity);
+    saveValues(x, temperatureToHumidity);
   }
   if (x.includes('humidity-to-location')) {
-    xToY(x, humidityToLocation);
+    saveValues(x, humidityToLocation);
   }
 })
 
+function fromXtoY(val, array) {
+  let finalRes = -1;
+  array.forEach(range => {
+    if (val >= range[1] && val < range[1] + range[2]) {
+      let delta = val - range[1];
+      let res = range[0] + delta;
+      finalRes = res;
+      return res; 
+    }
+  });
+  if (finalRes === -1) {
+    return val;
+  }
+  return finalRes;
+}
+
 function convertSeedToLocation(seed) {
   let curVal = seed;
-  if (seedToSoil.has(curVal)) {
-    curVal = seedToSoil.get(curVal);
-  }
-  if (soilToFertilizer.has(curVal)) {
-    curVal = soilToFertilizer.get(curVal);
-  }
-  if (fertilizerToWater.has(curVal)) {
-    curVal = fertilizerToWater.get(curVal);
-  }
-  if (waterToLight.has(curVal)) {
-    curVal = waterToLight.get(curVal);
-  }
-  if (lightToTemperature.has(curVal)) {
-    curVal = lightToTemperature.get(curVal);
-  }
-  if (temperatureToHumidity.has(curVal)) {
-    curVal = temperatureToHumidity.get(curVal);
-  }
-  if (humidityToLocation.has(curVal)) {
-    curVal = humidityToLocation.get(curVal);
-  }
+  curVal = fromXtoY(curVal, seedToSoil);
+  curVal = fromXtoY(curVal, soilToFertilizer);
+  curVal = fromXtoY(curVal, fertilizerToWater);
+  curVal = fromXtoY(curVal, waterToLight);
+  curVal = fromXtoY(curVal, lightToTemperature);
+  curVal = fromXtoY(curVal, temperatureToHumidity);
+  curVal = fromXtoY(curVal, humidityToLocation);
   return curVal;
 }
+
+
 
 let locations = [];
 for (i = 0; i < seeds.length; i++) {
   locations.push(convertSeedToLocation(seeds[i]));
 }
-// display min of locations
 console.log(Math.min(...locations));
--- day5/day5p2.js
+++ day5/day5p2.js
@@ -3,6 +3,87 @@ const input = require('fs').readFileSync('input.txt', 'utf8');
 // PART 1
 const input1 = require('fs').readFileSync('part1.txt', 'utf8');
 
-// find the line in the input where there is "seeds:"
-const seeds = input.split('\n').filter(x => x.includes('seeds:'));
-console.log(seeds);
+let seeds = [];
+let seedToSoil = [];
+let soilToFertilizer = [];
+let fertilizerToWater = [];
+let waterToLight = [];
+let lightToTemperature = [];
+let temperatureToHumidity = [];
+let humidityToLocation = [];
+
+function saveValues(x, array) {
+  let saveValuesMapValues = x.split(':')[1].split('\n').filter(x => x !== '');
+  saveValuesMapValues = saveValuesMapValues.map(x => x.split(' ').filter(x => x !== '').map(x => parseInt(x)));
+  saveValuesMapValues.forEach(x => {
+    array.push(x);
+  });
+}
+const part1 = input.split('\n\n').map(x=> {
+  if (x.includes('seeds:')) {
+    seeds = x.split(':')[1].split(' ').filter(x => x !== '').map(x => parseInt(x));
+  }
+  if (x.includes('seed-to-soil')) {
+    saveValues(x, seedToSoil);
+  }
+  if (x.includes('soil-to-fertilizer')) {
+    saveValues(x, soilToFertilizer);
+  }
+  if (x.includes('fertilizer-to-water')) {
+    saveValues(x, fertilizerToWater);
+  }
+  if (x.includes('water-to-light')) {
+    saveValues(x, waterToLight);
+  }
+  if (x.includes('light-to-temperature')) {
+    saveValues(x, lightToTemperature);
+  }
+  if (x.includes('temperature-to-humidity')) {
+    saveValues(x, temperatureToHumidity);
+  }
+  if (x.includes('humidity-to-location')) {
+    saveValues(x, humidityToLocation);
+  }
+})
+
+function fromXtoY(val, array) {
+  let finalRes = -1;
+  array.forEach(range => {
+    if (val >= range[1] && val < range[1] + range[2]) {
+      let delta = val - range[1];
+      let res = range[0] + delta;
+      finalRes = res;
+      return res; 
+    }
+  });
+  if (finalRes === -1) {
+    return val;
+  }
+  return finalRes;
+}
+
+function convertSeedToLocation(seed) {
+  let curVal = seed;
+  curVal = fromXtoY(curVal, seedToSoil);
+  curVal = fromXtoY(curVal, soilToFertilizer);
+  curVal = fromXtoY(curVal, fertilizerToWater);
+  curVal = fromXtoY(curVal, waterToLight);
+  curVal = fromXtoY(curVal, lightToTemperature);
+  curVal = fromXtoY(curVal, temperatureToHumidity);
+  curVal = fromXtoY(curVal, humidityToLocation);
+  return curVal;
+}
+
+
+
+let minLocation = 29189284711111111111111111111111111111;
+for (i = 0; i < seeds.length; i = i + 2) {
+  for (j = seeds[i]; j < seeds[i] + seeds[i + 1]; j++) {
+    let location = convertSeedToLocation(j);
+    if (location < minLocation) {
+      minLocation = location;
+    }
+  }
+}
+
+console.log(minLocation);
